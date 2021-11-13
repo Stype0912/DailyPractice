75e2c45f9521eede69725e449015f13288f974a4
Tang <57284539+Stype0912@users.noreply.github.com>
2021-11-13 22:45:02 +0800

    20211112

---
+++ climbing-stairs.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func climbStairs(n int) int {
+	ansList := []int{1, 2}
+	if n <= 2 {
+		return ansList[n-1]
+	}
+	for i := 2; i < n; i++ {
+		ansList = append(ansList, ansList[i-1]+ansList[i-2])
+	}
+	return ansList[n-1]
+}
+
+func main() {
+	fmt.Println(climbStairs(3))
+}
