5b545a3f8496aafbcf65e2ee52a4352df4801fdb
tangchenhao <tangchenhao.trainee@bytedance.com>
2021-11-18 20:06:38 +0800

    20211118

--- decode-the-slanted-ciphertext.go
+++ decode-the-slanted-ciphertext.go
@@ -13,18 +13,11 @@ func decodeCiphertext(encodedText string, rows int) string {
 		return strings.TrimRight(encodedText, " ")
 	}
 	lines := length / rows
-	matrix := make([][]string, rows)
-	for i := 0; i < rows; i++ {
-		matrix[i] = make([]string, lines)
-	}
-	for i := 0; i < length; i++ {
-		matrix[i/lines][i%lines] = string(encodedText[i])
-	}
 	ans := ""
 	i, j := 0, 0
 	head := j
 	for {
-		ans += matrix[i][j]
+		ans += string(encodedText[lines*i+j])
 		i++
 		j++
 		if i == rows {
