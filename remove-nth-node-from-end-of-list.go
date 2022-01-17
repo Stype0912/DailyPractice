1ec07c5fe6a309f504cbd75f952ce858ea30f5fd
tangchenhao <tangchenhao.trainee@bytedance.com>
2022-01-17 13:23:47 +0800

    0117

--- remove-nth-node-from-end-of-list.go
+++ remove-nth-node-from-end-of-list.go
@@ -1,9 +1,9 @@
 package main
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
 
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	dummy := &ListNode{0, head}
