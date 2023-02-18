aebe5f88cd9fa5830e9dccccdda51ef00fbab38b
Stype0912 <949021284@qq.com>
2023-02-18 16:15:04 +0800

    230218

---
+++ intersection-of-two-linked-lists-lcci/intersection-of-two-linked-lists-lcci.go
@@ -0,0 +1,19 @@
+package intersection_of_two_linked_lists_lcci
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	hashMap := map[*ListNode]bool{}
+	for tmp := headA; tmp != nil; tmp = tmp.Next {
+		hashMap[tmp] = true
+	}
+	for tmp := headB; tmp != nil; tmp = tmp.Next {
+		if hashMap[tmp] == true {
+			return tmp
+		}
+	}
+	return nil
+}
