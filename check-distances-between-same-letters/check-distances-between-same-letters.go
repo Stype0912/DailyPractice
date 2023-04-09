d03ee5cb3330624bb09b0c4ebe7795319ff01741
Stype0912 <949021284@qq.com>
2023-04-09 20:46:39 +0800

    230409

---
+++ check-distances-between-same-letters/check-distances-between-same-letters.go
@@ -0,0 +1,15 @@
+package check_distances_between_same_letters
+
+func checkDistances(s string, distance []int) bool {
+	letterMap := map[int32]int{}
+	for i, letter := range s {
+		if index, ok := letterMap[letter]; !ok {
+			letterMap[letter] = i
+		} else {
+			if i-index != distance[letter-'a'] {
+				return false
+			}
+		}
+	}
+	return true
+}
