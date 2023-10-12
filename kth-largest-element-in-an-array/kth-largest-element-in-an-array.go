0bbafe4570b113adbca203fc022f76b6567bb109
Stype0912 <949021284@qq.com>
2023-10-12 15:16:18 +0800

    231012

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
@@ -1,41 +1,29 @@
 package kth_largest_element_in_an_array
 
-import (
-	"math/rand"
-	"time"
-)
-
 func findKthLargest(nums []int, k int) int {
-	rand.Seed(time.Now().UnixNano())
-	return quickSelect(nums, 0, len(nums)-1, k)
+	n := len(nums)
+	return quickselect(nums, 0, n-1, n-k)
 }
 
-func quickSelect(arr []int, l, r, index int) int {
-	q := randomPartition(arr, l, r)
-	if q == index-1 {
-		return arr[q]
-	} else if q < index {
-		return quickSelect(arr, q+1, r, index)
-	} else {
-		return quickSelect(arr, l, q-1, index)
+func quickselect(nums []int, l, r, k int) int {
+	if l == r {
+		return nums[k]
 	}
-}
-
-func randomPartition(arr []int, l, r int) int {
-	i := rand.Int()%(r-l+1) + l
-	arr[i], arr[r] = arr[r], arr[i]
-	return partition(arr, l, r)
-}
-
-func partition(arr []int, l, r int) int {
-	x := arr[r]
+	partition := nums[l]
 	i := l - 1
-	for j := l; j < r; j++ {
-		if arr[j] >= x {
-			i++
-			arr[i], arr[j] = arr[j], arr[i]
+	j := r + 1
+	for i < j {
+		for i++; nums[i] < partition; i++ {
+		}
+		for j--; nums[j] > partition; j-- {
+		}
+		if i < j {
+			nums[i], nums[j] = nums[j], nums[i]
 		}
 	}
-	arr[i+1], arr[r] = arr[r], arr[i+1]
-	return i + 1
+	if k <= j {
+		return quickselect(nums, l, j, k)
+	} else {
+		return quickselect(nums, j+1, r, k)
+	}
 }
